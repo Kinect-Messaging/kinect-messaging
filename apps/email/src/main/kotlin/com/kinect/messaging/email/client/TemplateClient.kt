2f557d055486fa520cc736775b895e7cce49bc5e
Raj Palanisamy <>
2024-03-25 23:41:16 -0400

    fix issues in template client

--- apps/email/src/main/kotlin/com/kinect/messaging/email/client/TemplateClient.kt
+++ apps/email/src/main/kotlin/com/kinect/messaging/email/client/TemplateClient.kt
@@ -3,22 +3,35 @@ package com.kinect.messaging.email.client
 import com.kinect.messaging.libs.common.Defaults
 import com.kinect.messaging.libs.model.KTemplate
 import com.kinect.messaging.libs.model.TemplatePersonalizationRequest
+import io.netty.handler.logging.LogLevel
 import org.slf4j.MDC
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.client.reactive.ReactorClientHttpConnector
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.reactive.function.client.awaitBody
+import reactor.netty.http.client.HttpClient
+import reactor.netty.transport.logging.AdvancedByteBufFormat
 import java.util.UUID
+
+
+var httpClient = HttpClient
+    .create()
+    .wiretap("reactor.netty.http.client.HttpClient",
+        LogLevel.DEBUG, AdvancedByteBufFormat.TEXTUAL)
 
 @Component
 class TemplateClient (private val webClient: WebClient) {
+    @Value("\${app.client.template.apiKey}")
+    lateinit var apiKey: String
     suspend fun loadTemplate(personalizationRequest: TemplatePersonalizationRequest): List<KTemplate>? =
         webClient
             .post()
-            .bodyValue(personalizationRequest)
             .header(Defaults.TRANSACTION_ID_HEADER, MDC.get("transaction-id") ?: UUID.randomUUID().toString())
+            .header("Ocp-Apim-Subscription-Key", apiKey)
+            .bodyValue(personalizationRequest)
             .retrieve()
             .awaitBody<List<KTemplate>>()
 }
@@ -29,15 +42,10 @@ class Config{
     @Value("\${app.client.template.url}")
     lateinit var url: String
 
-    @Value("\${app.client.template.apiKey}")
-    lateinit var apiKey: String
-
     @Bean
     fun webClient(builder: WebClient.Builder): WebClient =
         builder
+            .clientConnector(ReactorClientHttpConnector(httpClient))
             .baseUrl(url)
-            .defaultHeaders { httpHeaders ->
-                httpHeaders.add("Ocp-Apim-Subscription-Key", apiKey)
-            }
             .build()
 }
