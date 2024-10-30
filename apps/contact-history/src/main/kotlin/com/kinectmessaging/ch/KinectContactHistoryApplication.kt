227714b6b562746bbfcca078b048b750005c7bbf
Raj Palanisamy <>
2024-10-30 18:21:07 -0400

    add instrumentation

--- apps/contact-history/src/main/kotlin/com/kinectmessaging/ch/KinectContactHistoryApplication.kt
+++ apps/contact-history/src/main/kotlin/com/kinectmessaging/ch/KinectContactHistoryApplication.kt
@@ -1,13 +1,21 @@
 package com.kinectmessaging.ch
 
+import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.metrics.buffering.BufferingApplicationStartup
 import org.springframework.boot.runApplication
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories
+
 
 @SpringBootApplication
 @EnableMongoRepositories("com.kinectmessaging.ch.repository")
 class KinectContactHistoryApplication
 
 fun main(args: Array<String>) {
-	runApplication<KinectContactHistoryApplication>(*args)
+	val app = SpringApplication(
+		KinectContactHistoryApplication::class.java
+	)
+	app.applicationStartup = BufferingApplicationStartup(2048)
+	app.run(*args)
+//	runApplication<KinectContactHistoryApplication>(*args)
 }
