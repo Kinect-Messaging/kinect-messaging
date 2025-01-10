c210524bd336605bad2f2d66e281b11150e0a6eb
Raj Palanisamy <>
2025-01-10 00:01:50 -0500

    add missing annotations

--- event-processor/src/main/kotlin/com/kinectmessaging/ep/client/NotificationClient.kt
+++ event-processor/src/main/kotlin/com/kinectmessaging/ep/client/NotificationClient.kt
@@ -1,7 +1,9 @@
 package com.kinectmessaging.ep.client
 
+import io.cloudevents.jackson.JsonFormat
 import io.quarkus.rest.client.reactive.NotBody
 import io.quarkus.rest.client.reactive.Url
+import jakarta.ws.rs.Consumes
 import jakarta.ws.rs.POST
 import org.eclipse.microprofile.rest.client.annotation.ClientHeaderParam
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient
@@ -9,6 +11,7 @@ import org.eclipse.microprofile.rest.client.inject.RegisterRestClient
 @RegisterRestClient(configKey = "notification-api")
 interface NotificationClient {
     @POST
+    @Consumes(JsonFormat.CONTENT_TYPE)
     @ClientHeaderParam(name = "aeg-sas-key", value = ["\${app.client.notification.access-key}"])
     fun sendNotification(
         @NotBody @Url url: String?,
